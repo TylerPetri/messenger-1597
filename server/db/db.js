6d0d0c79e4083593a1afc4b156739b31c4b7d410
Tyler Petri <tyler_1701@hotmail.com>
2021-08-19 20:14:09 -0400

    db sequelize connection, depedencies & git flow setup

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,24 @@
-const Sequelize = require("sequelize");
+require('dotenv').config({ path: '../.env' });
+const Sequelize = require('sequelize');
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://localhost:5432/messenger',
+  {
+    dialect: 'postgres',
+    username: 'postgres',
+    password: process.env.PSQL_PASSWORD,
+    logging: false,
+  }
+);
+
+async function connect() {
+  try {
+    await db.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+connect();
 
 module.exports = db;
