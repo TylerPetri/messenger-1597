8a0d89c7f961efcc622da158e111b49572189a37
Tyler Petri <tyler_1701@hotmail.com>
2021-08-21 17:30:32 -0400

    messages pushing to convo, need to update realtime

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -72,7 +72,6 @@ export const logout = (id) => async (dispatch) => {
 export const fetchConversations = () => async (dispatch) => {
   try {
     const { data } = await axios.get('/api/conversations');
-    console.log('props');
     dispatch(gotConversations(data));
   } catch (error) {
     console.error(error);
@@ -94,9 +93,10 @@ const sendMessage = (data, body) => {
 
 // message format to send: {recipientId, text, conversationId}
 // conversationId will be set to null if its a brand new conversation
-export const postMessage = (body) => (dispatch) => {
+export const postMessage = (body) => async (dispatch) => {
   try {
-    const data = saveMessage(body);
+    // const data = saveMessage(body);
+    const { data } = await axios.post('/api/messages', body);
 
     if (!body.conversationId) {
       dispatch(addConversation(body.recipientId, data.message));
