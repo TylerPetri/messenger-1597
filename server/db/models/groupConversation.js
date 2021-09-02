5c6037abe9003a9574aba9ffb31003e8d18a07d8
Tyler Petri <tyler_1701@hotmail.com>
2021-09-01 21:47:37 -0400

    groupConversation [Op.in] userId and recipients

--- server/db/models/groupConversation.js
+++ server/db/models/groupConversation.js
@@ -2,11 +2,12 @@ const db = require('../db');
 
 const GroupConversation = db.define('groupConversation', {});
 
-GroupConversation.findConversation = async function (userId) {
+GroupConversation.findConversation = async function (userId, recipients) {
   const groupConversation = await GroupConversation.findOne({
     where: {
       idArray: {
         [Op.in]: userId,
+        [Op.in]: recipients,
       },
     },
   });
